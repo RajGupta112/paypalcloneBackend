5eb74d732968755b8a8cf2a596c9900d59126d58
RajGupta112 <graj31105@gmail.com>
2025-10-18 18:23:04 +0530

    add kafka

---
+++ notification-service/src/main/java/com/paypal/notification_service/entity/Notification.java
@@ -0,0 +1,5 @@
+package com.paypal.notification_service.entity;
+
+public class Notification {
+
+}
--- transaction-service/src/main/java/com/paypal/transaction_service/service/TransactionServiceImplimentation.java
+++ transaction-service/src/main/java/com/paypal/transaction_service/service/TransactionServiceImplimentation.java
@@ -1,7 +1,9 @@
 package com.paypal.transaction_service.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paypal.transaction_service.entity.Transaction;
+import com.paypal.transaction_service.kafka.KafkaEventProducer;
 import com.paypal.transaction_service.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,8 @@ public class TransactionServiceImplimentation implements TransactionService {
 
     private  final ObjectMapper objectMapper;
 
+    private final KafkaEventProducer kafkaEventProducer;
+
     @Override
     public Transaction createTransaction(Transaction request) {
         Long senderId = request.getSenderId();
@@ -31,7 +35,20 @@ public class TransactionServiceImplimentation implements TransactionService {
         transaction.setStatus("SUCCESS");
 
         System.out.println("Incoming Transaction obejct:"+transaction);
-        return transactionRepository.save(transaction);
+        Transaction saved= transactionRepository.save(transaction);
+        System.out.println("Saved Transaction from DB:"+saved);
+
+        try{
+//          String evenPayload= objectMapper.writeValueAsString(saved);
+          String key= String.valueOf(saved.getId());
+          kafkaEventProducer.sendTransactionEvent(key,saved);
+            System.out.println("Kafka message sent");
+        } catch (Exception e) {
+            System.out.println("Failed to send Kafka event"+e.getMessage());
+            e.printStackTrace();
+        }
+
+        return  saved;
     }
 
     @Override
